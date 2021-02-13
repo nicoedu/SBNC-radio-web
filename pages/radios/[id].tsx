da7c2f5a7eb9906c08247edd6c1d7d7f3a397fb8
Danilo Lira <danilo.lira01@gmail.com>
2021-02-13 12:50:10 -0300

    Creates dynamic routes but with error

---
+++ pages/radios/[id].tsx
@@ -0,0 +1,26 @@
+import { getAllRadioIds, getRadioData } from '../../lib/radios'
+
+export default function Radio({radioData}): JSX.Element {
+  return (
+    <div>
+      <h1> radioData.name </h1>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllRadioIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const radioData = getRadioData(params.id)
+  return {
+    props: {
+      radioData
+    }
+  }
+}
