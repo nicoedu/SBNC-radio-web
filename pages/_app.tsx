8279fa29875b1efde2e8d042f09aa7054e97091a
Nicollas Bastos <nicollas.bastos@mambalabs.com.br>
2021-02-13 09:37:44 -0300

    update imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { AuthProvider } from '../auth'
 import type { AppProps /*, AppContext */ } from 'next/app'
 import theme from 'theme'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
   )
 }
