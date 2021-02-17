30e810792fca9657b61dd64e5f782711c98a20c2
Danilo Lira <danilo.lira01@gmail.com>
2021-02-17 20:59:32 -0300

    Styles the form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -6,49 +6,50 @@ import {
   Input,
   Textarea,
   Button,
-  Text,
-} from '@chakra-ui/react'
-import React from 'react'
+  Heading,
+  Box,
+} from "@chakra-ui/react";
+import React from "react";
 
 export default function ContactForm() {
   return (
     <form>
-      <VStack alignItems="start" ml={10}>
-        <Text fontSize="3xl" fontWeight="bold">
-          {' '}
+      <Box alignItems="start" ml={10}>
+        <Heading fontWeight="bold" mb={5} mt={3} align="left">
           Fale Conosco
-        </Text>
-        <HStack>
-          <FormControl id="first-name" isRequired>
+        </Heading>
+        <HStack mb={3}>
+          <FormControl id="first-name" isRequired w="45%" mr={5}>
             <FormLabel>Primeiro nome</FormLabel>
-            <Input placeholder="Primeiro Nome" />
+            <Input variant="flushed" placeholder="Primeiro Nome" />
           </FormControl>
 
-          <FormControl id="phone" isRequired>
+          <FormControl id="phone" isRequired w="45%">
             <FormLabel>Telefone</FormLabel>
-            <Input placeholder="+55(81)9999999" />
+            <Input variant="flushed" placeholder="+55(81)9999999" />
           </FormControl>
         </HStack>
 
-        <HStack>
-          <FormControl id="subject" isRequired>
+        <HStack mb={3}>
+          <FormControl id="subject" isRequired w="45%" mr={5}>
             <FormLabel>Assunto</FormLabel>
-            <Input placeholder="O assunto" />
+            <Input variant="flushed" placeholder="O assunto" />
           </FormControl>
 
-          <FormControl id="email" isRequired>
+          <FormControl id="email" isRequired w="45%">
             <FormLabel>E-mail</FormLabel>
-            <Input placeholder="Seu e-mail" type="email" />
+            <Input variant="flushed" placeholder="Seu e-mail" type="email" />
           </FormControl>
         </HStack>
 
-        <Textarea placeholder="Sua mensagem" />
+        <HStack mb={5}>
+          <Textarea placeholder="Sua mensagem" variant="flushed" w="95%" />
+        </HStack>
 
-        <Button colorScheme="blue" type="submit">
-          {' '}
-          Enviar{' '}
+        <Button colorScheme="blue" type="submit" w="30%">
+          Enviar
         </Button>
-      </VStack>
+      </Box>
     </form>
-  )
+  );
 }
