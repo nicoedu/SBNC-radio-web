9a5734076c668413024e125506ab51b3d0d688b4
Danilo Lira <danilo.lira01@gmail.com>
2021-02-17 16:39:47 -0300

    Some adjustments

--- pages/radios/[id].tsx
+++ pages/radios/[id].tsx
@@ -8,21 +8,21 @@ export default function Radio({radioData}): JSX.Element {
     <Box bgGradient="linear-gradient(180deg, #2E3092 0%, #D8B6B2 100%)" h="100vh" w="100%" align="center" justify="center">
       <Header />
       <HStack bg="rgba(255, 255, 255, 0.35)" backdrop-filter="blur(10px)" w="95vw" h="80vh" mt="3%" align="center" justify="center"> 
-        <Grid templateRows="repeat(3, 1fr)" templateColumns="repeat(4, 1fr)"  w="60vw" h="75vh" bg="white" mr="2%" >
+        <Grid templateRows="repeat(6, 1fr)" templateColumns="repeat(6, 1fr)"  w="60vw" h="75vh" bg="white" mr="2%" >
           
-          <GridItem rowSpan={1} colSpan={1}  border="1px solid" borderColor="pink.300">
+          <GridItem colStart={1} colEnd={3} rowStart={1} rowEnd={2} border="1px solid" borderColor="pink.300">
             <SocialMedia radioData={radioData} />
           </GridItem>
           
-          <GridItem colSpan={3} border="1px solid" borderColor="pink.300">
+          <GridItem colStart={3} colEnd={7} rowStart={1} rowEnd={3} border="1px solid" borderColor="pink.300">
             <Book radioData={radioData}/>
           </GridItem>
 
-          <GridItem rowSpan={2} colSpan={1} border="1px solid" borderColor="pink.300">
+          <GridItem rowSpan={2} colSpan={2} rowStart={2} rowEnd={7} border="1px solid" borderColor="pink.300">
             <AboutUS radioData={radioData} />
           </GridItem>
 
-          <GridItem colSpan={3} rowSpan={2} border="1px solid" borderColor="pink.300">
+          <GridItem colStart={3} colEnd={7} rowStart={3} rowEnd={7} border="1px solid" borderColor="pink.300">
             <ContactForm radioData={radioData}/>
           </GridItem>
         </Grid>
@@ -75,7 +75,7 @@ function SocialMedia({radioData}) {
         {radioData.name} 
       </Text>
 
-      <HStack align="center" mt={5}>
+      <HStack align="center" mt={5} mb={5}>
         <Link href={radioData.facebookLink} isExternal>
           <Image src="/facebook.svg" />
         </Link>
@@ -95,9 +95,9 @@ function SocialMedia({radioData}) {
 
 function AboutUS({radioData}) {
   return (
-    <Box alignItems="start" mt={5} ml={5}>
+    <Box alignItems="start" mt={5} w="80%">
       <Text fontWeight="bold" fontFamily="PT Sans Narrow, sans-serif" fontSize="2xl" align="start"> Sobre nós </Text>
-      <Text noOfLines={10} align="start" fontSize="sm">
+      <Text noOfLines={10} align="start" fontSize="sm" >
         {radioData.aboutUs}
       </Text>
       {/* TODO: Inserir carrossel de imagens */}
@@ -107,9 +107,9 @@ function AboutUS({radioData}) {
 
 function Book({radioData}) {
   return (
-    <Box alignItems="start" mt={5} ml={5}>
+    <Box alignItems="start" mt={5} ml={10}>
       <Text fontWeight="bold" fontFamily="PT Sans Narrow, sans-serif" fontSize="2xl" align="left"> Book </Text>
-      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width="70%" height="30vh" p={20}/>
+      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width="528px" height="300px" />
     </Box>
   );
 }
@@ -124,7 +124,7 @@ function ContactForm({radioData}) {
         </Text>
         
         <HStack w="90%" mb={3}>
-            <FormControl id="first-name" isRequired>
+          <FormControl id="first-name" isRequired mr={2}>
             <FormLabel>Primeiro nome</FormLabel>
             <Input variant="flushed" placeholder="Primeiro Nome" />
           </FormControl>
@@ -136,7 +136,7 @@ function ContactForm({radioData}) {
         </HStack>
 
         <HStack  w="90%" mb={3}>
-          <FormControl id="subject" isRequired>
+          <FormControl id="subject" isRequired mr={2}>
             <FormLabel>Assunto</FormLabel>
             <Input variant="flushed" placeholder="O assunto" />
           </FormControl>
@@ -170,8 +170,8 @@ function PriceTable({radioData}) {
       <Text fontWeight="bold" fontSize="2xl"> Tabela de Valores</Text>
       <Text fontSize="sm"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at lectus ultrices, volutpat ex at, gravida mi. Cras tristique tincidunt metus, vel luctus nulla consequat et.</Text>
 
-      <Button colorScheme="blue" w="80%"> Acessar Book </Button>
-      <Button colorScheme="green" w="80%"> Acessar Tabela </Button>
+      <Button colorScheme="blue" w="100%"> Acessar Book </Button>
+      <Button colorScheme="green" w="100%"> Acessar Tabela </Button>
     </VStack>
   );
 }
