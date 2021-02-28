7b4c04334672b2fd34773f8fd1afc3cb90ac9c06
Nicollas Bastos <nicollas.bastos@mambalabs.com.br>
2021-02-28 18:49:15 -0300

    centralize modal

--- components/ImagesModal.tsx
+++ components/ImagesModal.tsx
@@ -66,48 +66,52 @@ function ImagesModal({
         bg="darkBackground"
       >
         <ModalCloseButton color="white" />
-        <ModalBody display="flex" height="100%" align="center" jutify="center">
-          <Center>
-            <Flex flexDirection="row" align="center" justify="center">
-              <Link
-                pr={[1, 1, 5]}
-                color={'white'}
-                fontWeight={'bold'}
-                fontSize={'3rem'}
-                transition={' 0.6s ease'}
-                borderRadius={' 0 3px 3px 0'}
-                _hover={{ textDecoration: 'none' }}
-                onClick={() => {
-                  previousImage()
-                }}
-              >
-                &#10094;
-              </Link>
-              <Image
-                // m={'auto'}
-                maxW={'80%'}
-                maxH={'90vh'}
-                fallback={<Spacer />}
-                py={5}
-                src={images[imageIndex].src}
-                alt={images[imageIndex].name}
-              />
-              <Link
-                pl={[1, 1, 5]}
-                color={'white'}
-                fontWeight={'bold'}
-                fontSize={'3rem'}
-                transition={'0.6s ease'}
-                _hover={{ textDecoration: 'none' }}
-                borderRadius={'0 3px 3px 0'}
-                onClick={() => {
-                  nextImage()
-                }}
-              >
-                &#10095;
-              </Link>
-            </Flex>
-          </Center>
+        <ModalBody
+          display="flex"
+          height="100%"
+          align="center"
+          jutify="center"
+          mx={'auto'}
+        >
+          <Flex flexDirection="row" align="center" justify="center">
+            <Link
+              pr={[1, 1, 5]}
+              color={'white'}
+              fontWeight={'bold'}
+              fontSize={'3rem'}
+              transition={' 0.6s ease'}
+              borderRadius={' 0 3px 3px 0'}
+              _hover={{ textDecoration: 'none' }}
+              onClick={() => {
+                previousImage()
+              }}
+            >
+              &#10094;
+            </Link>
+            <Image
+              // m={'auto'}
+              maxW={'80%'}
+              maxH={'90vh'}
+              fallback={<Spacer />}
+              py={5}
+              src={images[imageIndex].src}
+              alt={images[imageIndex].name}
+            />
+            <Link
+              pl={[1, 1, 5]}
+              color={'white'}
+              fontWeight={'bold'}
+              fontSize={'3rem'}
+              transition={'0.6s ease'}
+              _hover={{ textDecoration: 'none' }}
+              borderRadius={'0 3px 3px 0'}
+              onClick={() => {
+                nextImage()
+              }}
+            >
+              &#10095;
+            </Link>
+          </Flex>
         </ModalBody>
       </ModalContent>
     </Modal>
