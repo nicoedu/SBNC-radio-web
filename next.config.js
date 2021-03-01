94611dc796580ced5e745b74e39dbae78699414c
Nicollas Bastos <nicollas.bastos@mambalabs.com.br>
2021-03-01 17:23:57 -0300

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/* eslint-disable */
+const withOptimizedImages = require('next-optimized-images')
+
+module.exports = withOptimizedImages({
+  defaultImageLoader: 'responsive-loader',
   target: 'serverless'
-}
+})
